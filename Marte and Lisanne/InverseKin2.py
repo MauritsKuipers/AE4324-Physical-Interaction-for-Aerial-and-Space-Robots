05fbb90d2249fd5ae4e6d4164ab301a18ec8be86
Mr-Medina <M.MedinaLeon@student.tudelft.nl>
2024-03-13 17:26:54 +0100

    I am a god

--- Marte and Lisanne/InverseKin2.py
+++ Marte and Lisanne/InverseKin2.py
@@ -1,34 +1,55 @@
 import numpy as np
+from Joint_plot import joint_plot
+def vertical_projection(x_global_ee, y_global_ee, z_global_ee):
+    theta_0 = np.arctan2(y_global_ee,x_global_ee) + np.deg2rad(90)
+    x_ee = -np.sqrt(x_global_ee**2 + y_global_ee**2)
+    y_ee = 0
+    z_ee = z_global_ee - 45 - 20
+    return theta_0, x_ee, y_ee, z_ee
 
-theta_ee = 0    #[rad]
-z_ee = 100      #[mm]
-y_ee = -100      #[mm]
-
-def theta_0(x_global_ee, y_global_ee):
-    theta_0 = np.arctan2(y_global_ee/x_global_ee) + np.pi/2
-    return theta_0
-def vertical_projection(x_global_ee, y_global_ee, z_global_ee, theta_0):
-    x_ee = 0
-    y_ee = -(x_global_ee/np.cos(np.pi/2 + theta_0))
-    z_ee = z_global_ee
-    return x_ee, y_ee, z_ee
-
-def joint_angles_projected(y_ee, z_ee, theta_ee):
+def joint_angles_projection(x_ee, z_ee, theta_ee):
 
     L_12 = 95       #[mm]
     L_23 = 105      #[mm]
     L_3ee = 75      #[mm]
 
-    y_3 = y_ee + L_3ee*np.cos(theta_ee)     #[mm]
-    z_3 = z_ee - L_3ee*np.sin(theta_ee)     #[mm]
-    L_13 = np.sqrt(z_3**2+y_3**2)           #[mm]
+    x_3 = x_ee + L_3ee*np.cos(theta_ee)     #[mm]
+    z_3 = z_ee + L_3ee*np.sin(theta_ee)     #[mm]
+    L_13 = np.sqrt(z_3**2+x_3**2)           #[mm]
 
-    theta_1 = np.arccos((L_12**2+L_13**2-L_23**2)/(2*L_12*L_13)) + np.arcsin(y_3/L_13)     #[rad]
+    theta_1 = np.arccos((L_12 ** 2 + L_13 ** 2 - L_23 ** 2) / (2 * L_12 * L_13)) + np.arctan2(z_3, x_3) - np.pi/2
 
     alpha_2 = np.arccos((L_12**2+L_23**2-L_13**2)/(2*L_12*L_23))     #[rad]
-    alpha_3 = np.arccos((L_13**2+L_23**2-L_12**2)/(2*L_13*L_23))     #[rad]
+#   alpha_3 = np.arccos((L_13**2+L_23**2-L_12**2)/(2*L_13*L_23))     #[rad]
 
-    theta_2 = np.pi - alpha_2     #[rad]
+    theta_2 = -(np.pi - alpha_2)     #[rad]
     theta_3 = np.pi/2 + theta_ee - theta_1 - theta_2     #[rad]
 
     return theta_1, theta_2, theta_3
+"""
+x = 0
+y = -95*np.sin(np.pi/4)
+z = 65+95*np.sin(np.pi/4)+105+75
+thetaee = np.deg2rad(-90)
+"""
+"""
+x = 0 #-y
+y = -105*np.sin(np.pi/4)#x
+z = 65+95+105*np.cos(np.pi/4) + 75#-z
+thetaee = np.deg2rad(-45)
+"""
+"""
+x = -242.7081528017131
+z = 74.14213562373097
+y = 0
+thetaee = np.deg2rad(45)
+"""
+x=100
+y=-100
+z=100
+thetaee = np.deg2rad(-90)
+
+theta_0, x_ee, y_ee, z_ee = vertical_projection(x,y,z)
+theta_1, theta_2, theta_3 = joint_angles_projection(x_ee, z_ee, thetaee)
+
+joint_plot(theta_0, theta_1, theta_2, theta_3, (x,y,z))
--- Marte and Lisanne/InverseKinematics.py
+++ Marte and Lisanne/InverseKinematics.py
@@ -49,14 +49,14 @@ sym.print_latex(sym.simplify(tab_0*t0_1*t2_1*t3_2*tee_3))
 # l0 = 0.19 # Distance from ground to the driven acces of J1
 # l1 = 1.132 # Length of the driven side of T1 from axle center to axle center
 # l2 = 0.14 # Distance between the driven side of T1 axle on J2 and the driven axle of T2 side of J2
-# l3 = 1.132 # Length of the driven side of T2 from axle center to axle center 
+# l3 = 1.132 # Length of the driven side of T2 from axle center to axle center
 # dx = 0.09163 # The distance between the axle center of the driven T2 side of J3 and the front of the faraday cage in the local x-direction (horizontal forward positive)
 # dy = -(0.10607 + 0.08673) # The distance between the axle center of the driven T2 side of J3 and the top of the faraday cage in the local y-direction (vertical downward positive)
 
-t0 = sym.Symbol('t0') 
-t1 = sym.Symbol('t1') 
-t2 = sym.Symbol('t2') 
-t3 = sym.Symbol('t3') 
+t0 = sym.Symbol('t0')
+t1 = sym.Symbol('t1')
+t2 = sym.Symbol('t2')
+t3 = sym.Symbol('t3')
 t4 = sym.Symbol('t4')
 tee = sym.Symbol('tee')
 
