c0a5e22c34d807adfa28895112131fd2ddc20b31
llcvermaas <101340255+llcvermaas@users.noreply.github.com>
2024-03-15 15:40:49 +0100

    Jacobian 3D added

--- Marte and Lisanne/Transformation.py
+++ Marte and Lisanne/Transformation.py
@@ -94,3 +94,20 @@ def transform_32EE(theta_4, link_32EE):
                    )
     return T34
 
+def jacobian(theta_0, theta_1, theta_2, theta_3, L12, L23, L3ee):
+    jacobian = np.array([[(L12*np.sin(theta_1) + L23*np.sin(theta_1 + theta_2) + L3ee*np.sin(theta_1 + theta_2 + theta_3))*np.cos(theta_0) ,
+ (L12*np.cos(theta_1) + L23*np.cos(theta_1 + theta_2) + L3ee*np.cos(theta_1 + theta_2 + theta_3))*np.sin(theta_0),
+ (L23*np.cos(theta_1+theta_2) + L3ee*np.cos(theta_1+theta_2+theta_3))*np.sin(theta_0) ,
+(L3ee*np.cos(theta_1+theta_2+theta_3))*np.sin(theta_0)],
+
+[(L12*np.sin(theta_1) + L23*np.sin(theta_1+theta_2) + L3ee*np.sin(theta_1+theta_2+theta_3))*np.sin(theta_0) ,
+ (L12*np.cos(theta_1) + L23*np.cos(theta_1+theta_2) + L3ee*np.cos(theta_1+theta_2+theta_3))*np.cos(theta_0) ,
+ -(L23*np.cos(theta_1+theta_2) + L3ee*np.cos(theta_1+theta_2+theta_3))*np.cos(theta_0) ,
+-(L3ee*np.cos(theta_1+theta_2+theta_3))*np.cos(theta_0)],
+
+[0 ,
+ -L12*np.sin(theta_1) - L23*np.sin(theta_1+theta_2) - L3ee*np.sin(theta_1+theta_2+theta_3),
+ -L23*np.sin(theta_1+theta_2) - L3ee*np.sin(theta_1+theta_2+theta_3) ,
+ - L3ee*np.sin(theta_1+theta_2+theta_3)]]
+                   )
+    return jacobian
