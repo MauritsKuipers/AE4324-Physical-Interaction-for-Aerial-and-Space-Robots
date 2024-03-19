17cc7d39abb129e013a7ed2139d73e5367b0eb42
llcvermaas <101340255+llcvermaas@users.noreply.github.com>
2024-03-19 11:31:39 +0100

    I found a nice circular trajectory in the robots workspace

--- Marte and Lisanne/Trajectory.py
+++ Marte and Lisanne/Trajectory.py
@@ -5,18 +5,18 @@ import matplotlib.pyplot as plt
 
 def trajectory_positions():
     # a circle discretized into 20 points
-    angles = np.linspace(0,360,3)
+    angles = np.linspace(0,360,20)
 
     # the ee positions
     x = []
     y = []
     z = []
 
-    radius = 50
+    radius = 10
     for i in angles:
-        x.append(radius + radius*np.cos(i*np.pi/180))
+        x.append(30 + radius + radius*np.cos(i*np.pi/180))
         y.append(0)
-        z.append(radius + radius*np.sin(i*np.pi/180))
+        z.append(30 + radius + radius*np.sin(i*np.pi/180))
 
     # plotting the points to check the shape
     plt.scatter(x,z)
